832df29d6385736ab43a33e4c02d54e380c19d6b
RoisyL <2059218150@qq.com>
2024-08-23 16:53:31 +0800

    用脚本获取ingress-address并尝试访问Service

---
+++ k3s/catch_ingressaddress.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 使用 kubectl 获取 Ingress 的 address
+INGRESS_ADDRESS=$(kubectl get ingress | grep nginx-ingress | awk '{print $4}')
+
+# 检查 INGRESS_ADDRESS 是否已获取
+if [ -z "$INGRESS_ADDRESS" ]; then
+    echo "Failed to get INGRESS_ADDRESS"
+    exit 1
+else
+    echo "Ingress_ADDRESS: $INGRESS_ADDRESS"
+fi
+
+# 使用获取的 INGRESS_ADDRESS 访问 Service
+curl "$INGRESS_ADDRESS"
